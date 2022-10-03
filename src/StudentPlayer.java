32edfcd7de14aacf15791398e6f50dbf60bb76a5
Patrik Csikós <pacsinta@gmail.com>
2022-10-03 12:52:57 +0200

    Init

---
+++ src/StudentPlayer.java
@@ -0,0 +1,10 @@
+public class StudentPlayer extends Player{
+    public StudentPlayer(int playerIndex, int[] boardSize, int nToConnect) {
+        super(playerIndex, boardSize, nToConnect);
+    }
+
+    @Override
+    public int step(Board board) {
+        return 0;
+    }
+}
