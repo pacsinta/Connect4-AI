030dc02bd96263e7f63edc841f34125d9375491f
Patrik Csikós <61376206+pacsinta@users.noreply.github.com>
2022-10-12 11:01:12 +0200

    work

--- src/StudentPlayer.java
+++ src/StudentPlayer.java
@@ -1,30 +1,136 @@
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Random;
+import java.util.*;
 
 public class StudentPlayer extends Player {
+    class Points {
+        public static final int inf = Integer.MAX_VALUE;
+        public static final int middle = 4;
+        public static final int lineoftwo = 2;
+        public static final int lineofthree = 5;
+    }
+
+    private static final int Depth = 10;
+
+    int middleRow;
+
     public StudentPlayer(int playerIndex, int[] boardSize, int nToConnect) {
         super(playerIndex, boardSize, nToConnect);
+        middleRow = boardSize[1] / 2;
+        if (boardSize[1] % 2 == 1) {
+            middleRow++;
+        }
     }
 
-    private int getValue(Board board) {
+    private int getValue(Board board, int step, int player) {
+        int value = 0;
+        Board copyBoard = new Board(board);
+        copyBoard.step(player, step);
 
-        //TODO
+        // win
+        if (copyBoard.gameEnded()) {
+            return Points.inf;
+        }
+
+        // middle points
+        if (step == middleRow) {
+            value += Points.middle;
+        }
+
+        // lines of two/three points
+        // first get the row
+        int[][] state = copyBoard.getState();
+        int row = 0;
+        for (int i = 6; i >= 0; i--) {
+            if (state[i][step] == player) {
+                row = i;
+                break;
+            }
+        }
+
+
+        for (int x = -3; x < 1; x++) {
+            for (int i = x; i < x + 4; i++) {
+                // test rows
+                boolean testrow = true;
+                try{
+                    if (testrow && (state[row][step + i] == player) && (i != 0)) {
+                        value += Points.lineoftwo;
+                    }
+                }catch (IndexOutOfBoundsException e){
+                    testrow = false;
+                }
+
+                // test columns
+                boolean testcol = true;
+                try{
+                    if (testcol && (state[row + i][step] == player) && (i != 0)){
+                        value += Points.lineoftwo;
+                    }
+                }catch (IndexOutOfBoundsException e){
+                    testcol = false;
+                }
+
+                // test diagonals
+                boolean testdiag1 = true;
+                try{
+                    if (testdiag1 && (state[row + i][step - i] == player) && (i != 0)){
+                        value += Points.lineoftwo;
+                    }
+                }catch (IndexOutOfBoundsException e){
+                    testdiag1 = false;
+                }
+
+                boolean testdiag2 = true;
+                try{
+                    if (testdiag2 && (state[row - i][step + i] == player) && (i != 0)){
+                        value += Points.lineoftwo;
+                    }
+                }catch (IndexOutOfBoundsException e){
+                    testdiag2 = false;
+                }
+            }
+        }
+
+
+        return value;
     }
 
-    private int minValue() {
-        //TODO
+    private int minValue(Board board) {
+        int min = Points.inf;
+        int step = 0;
+        for (int i = 0; i < boardSize[1]; i++) {
+            int value = getValue(board, i, this.playerIndex);
+            if(value < min){
+                min = value;
+                step = i;
+            }
+        }
+
+        return step;
     }
 
     private int maxValue(Board board) {
-        if (board.gameEnded()) return getValue();
+        int max = -Points.inf;
+        int step = 0;
+        for (int i = 0; i < boardSize[1]; i++) {
+            int value = getValue(board, i, this.playerIndex);
+            if(value > max){
+                max = value;
+                step = i;
+            }
+        }
 
+        return step;
     }
 
     @Override
     public int step(Board board) {
         //MINMAX algorithm
 
+        //create the graph of the game
+        int[][] valueGraph = new int[boardSize[1]][];
+        for(int i = 0; i < Depth; i++){
+
+        }
 
         //Create the game graph
         int depth = 5;
