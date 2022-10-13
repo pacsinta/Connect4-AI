6a17ab8a3d6ea557959dba97fb95a7ff4d5c0939
Patrik Csikós <61376206+pacsinta@users.noreply.github.com>
2022-10-13 18:26:24 +0200

    redo

--- src/StudentPlayer.java
+++ src/StudentPlayer.java
@@ -94,32 +94,53 @@ public class StudentPlayer extends Player {
         return value;
     }
 
-    private int minValue(Board board) {
-        int min = Points.inf;
-        int step = 0;
-        for (int i = 0; i < boardSize[1]; i++) {
-            int value = getValue(board, i, this.playerIndex);
-            if(value < min){
-                min = value;
-                step = i;
+    class Node {
+        Node[] nodes = new Node[boardSize[1]];
+        int value;
+        int step;
+        Board board;
+
+        Node(int value, Board board) {
+            this.value = value;
+            this.board = new Board(board);
+            board.getLastPlayerColumn();
+        }
+
+        Node(Board board) {
+            this.value = -1;
+            this.board = new Board(board);
+        }
+
+        void calc(boolean max) {
+            if(max){
+                for (int i = 0; i < boardSize[1]; i++) {
+                    if(nodes[i] != null){
+                        nodes[i].calc(false);
+
+                    }
+                }
             }
         }
 
-        return step;
-    }
+        // generate nodes for all the possible moves
+        void createChildren() {
+            int nextPlayer = 1;
+            if (board.getLastPlayerIndex() == 1) {
+                nextPlayer = 2;
 
-    private int maxValue(Board board) {
-        int max = -Points.inf;
-        int step = 0;
-        for (int i = 0; i < boardSize[1]; i++) {
-            int value = getValue(board, i, this.playerIndex);
-            if(value > max){
-                max = value;
-                step = i;
+                for (int i = 0; i < boardSize[1]; i++) {
+                    Board copyBoard = new Board(board);
+                    if(copyBoard.stepIsValid(i)){
+                        int moveValue = getValue(copyBoard, i, nextPlayer);
+
+                        copyBoard.step(nextPlayer, i);
+                        nodes[i] = new Node(moveValue, copyBoard);
+                    }else{
+                        nodes[i] = null;
+                    }
+                }
             }
         }
-
-        return step;
     }
 
     @Override
@@ -127,86 +148,14 @@ public class StudentPlayer extends Player {
         //MINMAX algorithm
 
         //create the graph of the game
-        int[][] valueGraph = new int[boardSize[1]][];
+        Node root = new Node(board);
+
         for(int i = 0; i < Depth; i++){
-
+            root.createChildren();
         }
 
-        //Create the game graph
-        int depth = 5;
-        List<ArrayList<Board>> graph = new ArrayList<>();
+        root.calc(playerIndex);
 
-        try {
-            graph.add(nextStates(board));
-        } catch (ExceptionReturn e) {
-            //TODO: handle exception
-        }
-
-        int max = 0;  // best found score
-        int row = 0;
-        int col = 0;
-
-        for (int i = 0; i < depth; i++) {
-            for (Board b : graph.get(i)) {
-                try {
-                    ArrayList<Board> next = nextStates(b);
-                    graph.add(next);
-                } catch (ExceptionReturn e) {
-                    //TODO: handle exception
-                }
-
-            }
-        }
-
-        Random rand = new Random();
-        return rand.nextInt(boardSize[1]);
-    }
-
-    private int minmaxAlgorithm(Board board, int depth) {
-        maxValue(board, depth);
-    }
-
-    private int getValue(Board board) {
-
-    }
-
-    private int maxValue(Board board, int depth) {
-        if (board.gameEnded()) return -1;
-
-        ArrayList<Board> nextStates = nextStates(board);
-        for (Board b : nextStates) {
-            return Math.max(v, minValue(b, depth - 1));
-        }
-    }
-
-    private int minValue(Board board, int depth) {
-        if (board.gameEnded()) return -2;
-
-        ArrayList<Board> nextStates = nextStates(board);
-        for (Board b : nextStates) {
-            maxValue(b, depth - 1);
-        }
-    }
-
-
-    private static class ExceptionReturn extends Exception {
-        public Board board;
-
-        ExceptionReturn(Board message) {
-            this.board = message;
-        }
-    }
-
-    private ArrayList<Board> nextStates(Board board) {
-        ArrayList<Integer> validSteps = board.getValidSteps();
-        ArrayList<Board> nextStates = new ArrayList<>();
-
-        for (Integer validStep : validSteps) {
-            Board newBoard = new Board(board);
-            newBoard.step(playerIndex, validStep);
-            nextStates.add(newBoard);
-        }
-
-        return nextStates;
+        return root.step;
     }
 }
